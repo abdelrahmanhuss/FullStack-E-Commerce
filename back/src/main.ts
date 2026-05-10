60da3c21760e83ded91696b6f75086fab014bea2
abvbdelrahman <98822056+abvbdelrahman@users.noreply.github.com>
2026-05-10 04:09:27 +0300

    Update main.ts

--- back/src/main.ts
+++ back/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   // تفعيل CORS
   app.enableCors({
-    origin: 'http://localhost:5173', // عنوان Frontend (Vite)
+    origin: process.env.FRONTEND_URL, // عنوان Frontend (Vite)
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
