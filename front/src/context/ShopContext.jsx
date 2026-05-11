6a82ab9fda18fcbbb4bcc0eabdc9021a7eb33050
abvbdelrahman <your-email@example.com>
2026-05-12 01:08:35 +0300

    عقم

--- front/src/context/ShopContext.jsx
+++ front/src/context/ShopContext.jsx
@@ -6,7 +6,7 @@ export const ShopContext = createContext();
 
 const ShopContextProvider = ({children})=>{
 
-    const url = "http://localhost:4000";
+    const url = "https://full-stack-e-commerce-liard.vercel.app/";
 
     const [cartItems,setCartItems] = useState(() => {
         const savedCart = localStorage.getItem("cartItems");
